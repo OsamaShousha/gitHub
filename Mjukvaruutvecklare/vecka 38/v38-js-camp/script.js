85913f810cb98fa91d40061ed8392272504813e7
Osama <osamashousha@hotmail.com>
2026-09-15 15:12:01 +0200

    feat: introduce const/let and basic types

--- Mjukvaruutvecklare/vecka 38/v38-js-camp/script.js
+++ Mjukvaruutvecklare/vecka 38/v38-js-camp/script.js
@@ -3,3 +3,13 @@ console.log("Aurora Live");
 console.log("Aurora Live");
 console.log("Aurora Live");
 console.log("Nord Glow");
+
+const eventName = "eget eventnamn ";
+let ticketCount = 0;
+console.log(eventName);
+console.log(eventName);
+ticketCount = 10;
+console.log(ticketCount);
+const isVip = false;
+console.log(isVip);
+ eventName = "Nord Glow";
